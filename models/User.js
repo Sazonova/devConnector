bc10e39e336528c5274cd4ef538ff199bc5efcb3
Kateryna <sazonova.k.y@gmail.com>
2019-12-04 23:41:34 +0200

    backend

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        uniqueCaseInsensitive: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+UserSchema.plugin(uniqueValidator);
+
+module.exports = User = mongoose.model('user', UserSchema);
